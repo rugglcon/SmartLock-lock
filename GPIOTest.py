f40ecf653b17ce540c5a6f9ea878b731736c0337
bmeeder22 <bmeeder22@gmail.com>
2017-10-31 18:39:12 +0000

    added an exmaple file for GPIO output

---
+++ GPIOTest.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(03, GPIO.OUT)
+
+pwm=GPIO.PWM(03, 50)
+
+pwm.start(0)
+
+def SetAngle(angle):
+    duty = angle /18 + 2
+    GPIO.output(03, True)
+    pwm.ChangeDutyCycle(duty)
+    sleep(1)
+    GPIO.output(03, False)
+    pwm.ChangeDutyCycle(0)
+
+def lock():
+    SetAngle(0)
+
+def unlock():
+    SetAngle(180)
+
+lock()
+sleep(1)
+unlock()
+
+pwm.stop()
+GPIO.cleanup()
